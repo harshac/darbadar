7ad281cc94e7bffa80bb672c8cc32a06583fd035
Harsha Chaudhary <harsha.chaudhary@gmail.com>
2015-09-02 23:03:43 +0530

    Adding logic for gallery traversal

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,7 +1,30 @@
 // Image click
 $('.post-content img').click(function(){
   var name=$(this).attr("name")
-  var url=$($(".gallery-list li img[name=" + name + "]")).attr("src");
+  loadGallery(name)
+});
+
+$(".prev-img").click(function(){
+  var name=$(this).attr("name")
+  loadGallery(name)
+})
+
+$(".next-img").click(function(){
+  var name=$(this).attr("name")
+  loadGallery(name)
+})
+
+function loadGallery(name){
+  var listOfImages=$(".gallery-list img")
+  var currentImage=$(".gallery-list li img[name=" + name + "]")
+  var url=$(currentImage).attr("src");
+  var currentImageIndex = listOfImages.index(currentImage)
+  if(currentImageIndex>0){
+  	$('.overlay .prev-img').attr("name", $(listOfImages[currentImageIndex-1]).attr("name"))
+  }
+  if(currentImageIndex< listOfImages.length){
+  	$('.overlay .next-img').attr("name", $(listOfImages[currentImageIndex+1]).attr("name"))	
+  }  
   $(".overlay img").attr("src", url);
   $(".overlay").removeClass("hide");
-});
+}
