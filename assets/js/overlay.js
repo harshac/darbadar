9e427b7c327aabee759e1f06dbad73f88d21a020
praveen <apnerve@gmail.com>
2015-09-09 10:33:49 +0530

    slight enhancements in gallery

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,40 +1,3 @@
-// Image click
-$('.post-content img').click(function(){
-  var name=$(this).attr("name")
-  loadGallery(name)
-});
-
-$(".previous-image").click(function(){
-  var name=$(this).attr("name")
-  loadGallery(name)
-})
-
-$(".next-image").click(function(){
-  var name=$(this).attr("name")
-  loadGallery(name)
-})
-
-$(".post-header").click(function(){
-	loadGallery("feature-image")
-})
-
-$(document).keydown(function(e){
-    if(e.which == 27)
-    {
-      closeGallery()
-    } else if(e.which == 37){
-    	loadGallery($(".previous-image").attr("name"))
-	} else if(e.which == 39)
-    {
-      loadGallery($(".next-image").attr("name"))
-    }
-})
-
-
-$(".close-button").click(function(){
-	closeGallery();
-})
-
 function closeGallery(){
 	$(".overlay").addClass("hide");	
 	$("body").removeClass("modal-open");
@@ -57,12 +20,55 @@ function loadGallery(name){
   $("body").addClass("modal-open");
 }
 
-$(document).ready(function(){
-  $.getJSON("https://api.flickr.com/services/rest/?&format=json&jsoncallback=?&api_key=cfff126f86dcd7009dbce5fe2e253f57&method=flickr.photosets.getPhotos&extras=url_t,url_m,url_o,url_s,url_l,url_z,description&photoset_id=" + $("#albumId").text().trim(),
+var showGallery = function(album_id) {
+  $.getJSON("https://api.flickr.com/services/rest/?&format=json&jsoncallback=?&api_key=cfff126f86dcd7009dbce5fe2e253f57&method=flickr.photosets.getPhotos&extras=url_t,url_m,url_o,url_s,url_l,url_z,description&photoset_id=" + album_id,
     function(data){
       $.each(data.photoset.photo, function(index, value){
         $(".gallery-list").append("<li class='gallery-item' src=\"" + value.url_l + "\" name=\"" + value.title + "\" data-caption=\"" + value.description._content + "\" index=\"" + index + "\"></li>");
       })
-      
     });
+}
+
+$(function() {
+  var album_id = $('.post').data('album-id');
+  if (!!album_id) {
+    showGallery(album_id);
+
+    // Image click
+    $('.post-content img').click(function(){
+      var name=$(this).attr("name")
+      loadGallery(name)
+    });
+
+    $(".previous-image").click(function(){
+      var name=$(this).attr("name")
+      loadGallery(name)
+    })
+
+    $(".next-image").click(function(){
+      var name=$(this).attr("name")
+      loadGallery(name)
+    })
+
+    $(".post-header").click(function(){
+      loadGallery("feature-image")
+    })
+
+    $(document).keydown(function(e){
+      if(e.which == 27)
+      {
+        closeGallery()
+      } else if(e.which == 37){
+       loadGallery($(".previous-image").attr("name"))
+     } else if(e.which == 39)
+     {
+      loadGallery($(".next-image").attr("name"))
+    }
+  })
+
+
+    $(".close-button").click(function(){
+      closeGallery();
+    })
+  }
 })
