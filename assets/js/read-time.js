18112f0ddb26959a47160dc21b5d279b1533749f
praveen <apnerve@gmail.com>
2016-04-06 00:35:59 +0530

    read time styling and put JS in a IIFE

--- assets/js/read-time.js
+++ assets/js/read-time.js
@@ -1,20 +1,22 @@
-function wordsPerMinute(body) {
-    var text = $(body).text();  
-    var wordCount = getWordCount(text);
-    return Math.ceil(wordCount/300);
-}
+(function($) {
+    function wordsPerMinute(body) {
+        var text = $(body).text();
+        var wordCount = getWordCount(text);
+        return Math.ceil(wordCount / 300);
+    }
 
-function getWordCount(text) {
-  return text.split(" ").length;
-}
+    function getWordCount(text) {
+        return text.split(" ").length;
+    }
 
-function setReadTime(body) {
-  var readTime = wordsPerMinute(body);
-  var unit = readTime == 1 ? "min" : "mins";
-  $('.post-read-time').append(readTime + " " + unit + " read");
-}
+    function setReadTime(body) {
+        var readTime = wordsPerMinute(body);
+        var unit = readTime == 1 ? "min" : "mins";
+        $('.post-read-time').append(readTime + " " + unit + " read");
+    }
 
-$(function() {
-  var body = $('.post-body');
-  setReadTime(body);
-})
+    $(function() {
+        var body = $('.post-body');
+        setReadTime(body);
+    });
+})(jQuery);
